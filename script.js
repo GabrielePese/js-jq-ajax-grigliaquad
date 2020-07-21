41bb53cb6ebabdd53614a2307f5fd70e8dc12608
Gabriele <gabrielepese@gmail.com>
2020-07-21 16:11:09 +0200

    Incredibile ma ce l'ho fatta al primo colpo!

---
+++ script.js
@@ -0,0 +1,43 @@
+
+// Griglia 6x6, ad ogni click parte una
+// richiesta AJAX che prende un
+// numero random da 1 a 9.
+// Se è <= 5 il quadrato diventa giallo,
+// se è > di 5 il quadrato diventa verde.
+// Il numero ottenuto appare al centro
+// del quadrato
+// (vedi una slide di oggi)
+
+
+$(document).ready(function(){
+  $(".blocco").click(function(){
+    var target = $(this)
+    $.ajax({
+      url: "https://flynn.boolean.careers/exercises/api/random/int",
+      method: "GET",
+      success: function(data, state){
+
+        var success = data["success"]
+        var value = data["response"]
+
+
+        if (value % 2 == 0) {
+
+          $(target).addClass("color")
+          $(target).text(value)
+
+        }
+        else {
+          $(target).addClass("color2")
+          $(target).text(value)
+        }
+      }
+
+
+
+    }
+  )
+
+
+  })
+})
